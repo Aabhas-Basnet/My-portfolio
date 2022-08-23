fc1548ffd4941dfc81710639d7cd569aefdcf695
unknown <habullaemote@gmail.com>
2022-08-23 14:43:37 +0545

    Update

--- CSS/JS/Nav.js
+++ CSS/JS/Nav.js
@@ -20,3 +20,58 @@ function menutoggle(){
  }
 }
 
+
+jQuery(function($){
+	var topMenuHeight = $("#desktop-nav").outerHeight();
+	$("#desktop-nav").menuScroll(topMenuHeight);
+});
+
+jQuery.fn.extend({
+    menuScroll: function(offset) {
+        var topMenu = this;
+		var topOffset = offset ? offset : 0;
+        var menuItems = $(topMenu).find("a");
+        var lastId;
+	
+        var scrollItems = $(menuItems).map(function() {
+            var item = $($(this).attr("href"));
+            if (item.length) {
+                return item;
+            }
+        });
+
+        $(topMenu).on("click", "a", function(e){
+            var href = $(this).attr("href");
+            
+            var offsetTop = href === "#" ? 0 : $(href).offset().top-topOffset;
+
+            $('html, body').stop().animate({ 
+                scrollTop: offsetTop
+            }, 300);
+            e.preventDefault();
+
+        });
+		
+        $(window).scroll(function(){
+            var nm = $("html").scrollTop();
+            var nw = $("body").scrollTop();
+            var fromTop = (nm > nw ? nm : nw)+topOffset;
+
+			
+            var current = $(scrollItems).map(function(){
+                if ($(this).offset().top <= fromTop)
+                return this;
+            });
+			
+            current = current[current.length-1];
+            var id = current && current.length ? current[0].id : "";
+            if (lastId !== id) {
+                lastId = id;
+                $(menuItems)
+                .parent().removeClass("active")
+                .end().filter("[href='#"+id+"']").parent().addClass("active");
+            }      
+
+        });
+    }
+});
